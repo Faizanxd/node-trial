c2d07b832bd8d68666e93ab0dbb2d5cb60e7f4f2
Faizanxd <fs8167@gmail.com>
2023-02-19 18:14:11 +0530

    xd

--- node-trial/src/components/header.tsx
+++ node-trial/src/components/header.tsx
@@ -1,33 +1,43 @@
+import { useRef } from "react";
+
 export default function Header() {
+  const headerRef = useRef<HTMLDivElement>(null);
+
   return (
-    <header>
-      <div className="container mx-auto flex flex-col flex-wrap items-center px-5 py-5 pb-5 md:flex-row md:justify-around">
-        <a className="flex text-gray-900 " href="/">
-          <span className="rounded-xl bg-newyellow p-[15px] font-Poppins text-3xl font-bold text-white">
-            Eatwell
-          </span>
-        </a>
-        <nav className=" flex flex-wrap items-center justify-center text-2xl md:justify-evenly">
-          <a
-            className="mr-5 rounded-md p-[4px] text-morebrown  hover:bg-lightbrown"
-            href="/browse"
-          >
-            Browse Restaurants
+    <>
+      <header>
+        <div
+          ref={headerRef}
+          className="flex flex-row justify-around p-4 md:container  md:items-center "
+        >
+          <a className="flex text-gray-900 " href="/">
+            <span className="rounded-xl bg-newyellow p-[15px] font-Poppins text-3xl font-bold text-white">
+              Eatwell
+            </span>
           </a>
-          <a
-            className="mr-5 rounded-md p-[4px] text-morebrown  hover:bg-lightbrown"
-            href="/share"
-          >
-            Share a Restaurant
-          </a>
-          <a
-            className="mr-5 rounded-md p-[4px] text-morebrown  hover:bg-lightbrown"
-            href="/about"
-          >
-            About Eatwell
-          </a>
-        </nav>
-      </div>
-    </header>
+
+          <nav className="hidden md:flex md:flex-wrap md:items-center  md:justify-evenly md:text-2xl">
+            <a
+              className="mr-5 rounded-md p-[4px] text-morebrown  hover:bg-lightbrown"
+              href="/browse"
+            >
+              Browse Restaurants
+            </a>
+            <a
+              className="mr-5 rounded-md p-[4px] text-morebrown  hover:bg-lightbrown"
+              href="/share"
+            >
+              Share a Restaurant
+            </a>
+            <a
+              className="mr-5 rounded-md p-[4px] text-morebrown  hover:bg-lightbrown"
+              href="/about"
+            >
+              About Eatwell
+            </a>
+          </nav>
+        </div>
+      </header>
+    </>
   );
 }
--- node-trial/src/components/layout.tsx
+++ node-trial/src/components/layout.tsx
@@ -1,10 +1,11 @@
 import Header from "./header";
 import { Outlet } from "react-router-dom";
-  
+
 export default function Layout() {
   return (
     <>
       <Header />
+
       <main>
         <Outlet />
       </main>
