6dcf437bf4910613562ece530b75059bb7b86631
Faizanxd <fs8167@gmail.com>
2023-02-26 19:49:22 +0530

    crud mongodb

---
+++ mongodb/app.js
@@ -0,0 +1,22 @@
+const path = require("path");
+const express = require("express");
+const blogRoutes = require("./routes/blog");
+const app = express();
+const db = require("./data/database");
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.use(blogRoutes);
+
+app.use(function (error, next, req, res) {
+  console.log(error);
+  res.status(500).send("500");
+});
+
+db.connectToDatabase().then(() => {
+  app.listen(3000);
+});
