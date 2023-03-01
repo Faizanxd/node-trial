f60c27b023901d1a52b8ef3e5d14253782634866
Faizanxd <fs8167@gmail.com>
2023-03-01 21:22:07 +0530

    base login

---
+++ user-auth/routes/demo.js
@@ -0,0 +1,85 @@
+const express = require("express");
+const bcrypt = require("bcryptjs");
+const db = require("../data/database");
+
+const router = express.Router();
+
+router.get("/", function (req, res) {
+  res.render("welcome");
+});
+
+router.get("/signup", function (req, res) {
+  res.render("signup");
+});
+
+router.get("/login", function (req, res) {
+  res.render("login");
+});
+
+router.post("/signup", async function (req, res) {
+  const userData = req.body;
+  const enteredEmail = userData.email;
+  const enteredConfirmEmail = userData["confirm-email"];
+  const enteredPassword = userData.password;
+
+  if (
+    !enteredEmail ||
+    !enteredConfirmEmail ||
+    !enteredPassword ||
+    enteredEmail !== enteredConfirmEmail ||
+    enteredPassword.trim() < 6 ||
+    !enteredPassword.includes("@")
+  ) {
+    return res.redirect("/signup");
+  }
+
+  const existingUser = await db
+    .getDb()
+    .collection("users")
+    .findOne({ email: enteredEmail });
+
+  if (existingUser) {
+    return res.redirect("/signup");
+  }
+
+  const hashedPassword = await bcrypt.hash(enteredPassword, 12);
+
+  const user = {
+    email: enteredEmail,
+    password: hashedPassword,
+  };
+
+  await db.getDb().collection("users").insertOne(user);
+  res.redirect("/login");
+});
+
+router.post("/login", async function (req, res) {
+  const userData = req.body;
+  const enteredEmail = userData.email;
+  const enteredPassword = userData.password;
+
+  const user = await db
+    .getDb()
+    .collection("users")
+    .findOne({ email: enteredEmail });
+
+  if (!user) {
+    return res.redirect("/signup");
+  }
+
+  const passwordMatch = await bcrypt.compare(enteredPassword, user.password);
+
+  if (!passwordMatch) {
+    return res.redirect("/signup");
+  }
+
+  res.redirect("/admin");
+});
+
+router.get("/admin", function (req, res) {
+  res.render("admin");
+});
+
+router.post("/logout", function (req, res) {});
+
+module.exports = router;
